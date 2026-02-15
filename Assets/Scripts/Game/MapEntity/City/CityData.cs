5c3ded2594c63bee01c4704f258949e94f695e40
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-02-16 02:06:30 +0300

    Add CityState storage to CityData

--- Assets/Scripts/Game/MapEntity/City/CityData.cs
+++ Assets/Scripts/Game/MapEntity/City/CityData.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using Game.Simulation;
 using UnityEngine;
 
 namespace Game.Entity
 {
+    [CreateAssetMenu(fileName = "CityData", menuName = "Game/Entity/City Data")]
     public class CityData : MapEntityData
     {
-        
+        [SerializeField] private CityState cityState = new();
+
+        public CityState CityState => cityState;
+
+        public void SetCityState(CityState state)
+        {
+            cityState = state ?? new CityState();
+        }
     }
 }
